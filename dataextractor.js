5bf69a89c565222fd456d7cff486092eec127b19
Pierre Racine <pierre.racine@sbf.ulaval.ca>
2023-02-21 14:38:13 -0500

    Added buttons to delete field definition rows

--- dataextractor.js
+++ dataextractor.js
@@ -108,30 +108,62 @@ var prepareExtract = () => {
 };
 
 var addRow = (el) => {
-  var newRow = document.getElementById('firstFieldDefsRow').cloneNode(true);
-  newRow.removeAttribute('id');
-  // reset the inputs
+  var newRow = document.getElementsByClassName("fieldDefsRow")[0].cloneNode(true);
+
+  // reset the inputs and add the onchange event listener
   var changingfields = newRow.getElementsByClassName("changingfield");
   Array.from(changingfields).forEach(function(element) {
     element.addEventListener('input', prepareExtract);
     element.value = "";
   });
 
+  // add the delete event listener
+  var deleteButtons = newRow.getElementsByClassName('deleteRowButton');
+  Array.from(deleteButtons).forEach(function(element) {
+    element.addEventListener('click', deleteRow);
+  });
+
   var target = document.getElementById('fieldDefsRows');
   target.appendChild(newRow);
+
   // increment the field name
   newRow.querySelector("input[name='fieldname']").value = "field" + target.childElementCount;
+
+  // enable the delete row button
+  var allDeletebuttons = document.getElementsByClassName('deleteRowButton');
+  Array.from(allDeletebuttons).forEach(function(element) {
+    element.removeAttribute('disabled');
+  });
 
   prepareExtract();
 }
 
-var changingfields = document.getElementsByClassName("changingfield");
+var deleteRow = (el) => {
+  var allDeletebuttons = document.getElementsByClassName('deleteRowButton');
+  if (allDeletebuttons.length == 2) {
+    Array.from(allDeletebuttons).forEach(function(element) {
+      element.setAttribute('disabled', true);
+    });
+  }
+  if (allDeletebuttons.length > 1) {
+    el.currentTarget.closest(".fieldDefsRow").remove();
+  }
+}
 
+// add onchange listener to all fields
+var changingfields = document.getElementsByClassName("changingfield");
 Array.from(changingfields).forEach(function(element) {
   element.addEventListener('input', prepareExtract);
 });
 
+// add click listener to add buttons
 document.getElementById('addRowButton').addEventListener("click", addRow);
+
+// add click listener to delete buttons
+var deleteButtons = document.getElementsByClassName('deleteRowButton');
+Array.from(deleteButtons).forEach(function(element) {
+  element.addEventListener('click', deleteRow);
+});
 
 document.getElementById("sourceinput").value = "<row><p>data1</p><p>data2</p></row><row><p>data3</p><p>data4</p></row>";
 
